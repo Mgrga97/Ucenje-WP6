b01f532a7d651b782aa457d72a77c06db597e103
Marko grgić <marko.grg97@gmail.com>
2024-11-13 18:25:01 +0100

    gotov zoo s tablicama

--- zoo.sql
+++ zoo.sql
@@ -10,11 +10,33 @@ go
 use zoo;
 go
 
-create table zivotinja (
+create table prostorija(
+sifra int not null primary key identity(1,1),
+dimenzije varchar(30) not null,
+maks_broj int not null,
+mjesto varchar(30) not null,
+);
+
+create table datum(
+d_rodenja datetime not null,
+d_dolaska datetime not null,
+d_smrti datetime not null,
+sifra int not null primary key identity(1,1)
+);
+
+create table djelatnik(
+sifra int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(50) not null,
+iban varchar(50) not null
+);
+
+create table zivotinja(
 sifra int not null primary key identity(1,1),
 vrsta varchar(50) not null,
 ime varchar(50) not null,
-djelatnik int not null primary key identity(1,1),
-prostorija int,
-datum int,
+djelatnik int references djelatnik(sifra) not null,
+prostorija int not null,
+datum int references datum(sifra) not null
 );
+
