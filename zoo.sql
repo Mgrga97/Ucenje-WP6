95370e4737199932d5cf5f40564b2e26c425666b
Marko grgić <marko.grg97@gmail.com>
2024-11-13 17:59:01 +0100

    Rad na zoo bazi

--- zoo.sql
+++ zoo.sql
@@ -1,10 +1,20 @@
+use master;
+go
+
+drop database if exists zoo;
+go
+--kreiram novu bazu
 create database zoo;
+go
+
 use zoo;
+go
+
 create table zivotinja (
-sifra int,
-vrsta varchar(50),
-ime varchar(50),
-djelatnik int,
+sifra int not null primary key identity(1,1),
+vrsta varchar(50) not null,
+ime varchar(50) not null,
+djelatnik int not null primary key identity(1,1),
 prostorija int,
 datum int,
 );
