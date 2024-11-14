d9ae957e9bc331607f0e6fc41988d02bb274c4ac
Mgrga97 <marko.grg97@gmail.com>
2024-11-14 21:46:46 +0100

    dokumentacija br2

--- skriptawp6.sql
+++ skriptawp6.sql
@@ -1,22 +1,21 @@
 ﻿-- Ovo je SQL skripta
 -- prebacujem se na master bazi
-
 use master;
-go
-
+go -- dajemo mu vremena da se prebaci prije nego što ide dalje
+-- brišem postojeću bazu ako postoji
 drop database if exists edunovawp6;
 go
---kreiram novu bazu
+-- kreiram novu bazu
 create database edunovawp6;
 go
-
+-- pozicioniram se na bazu
 use edunovawp6;
 go
-
+-- kreiram tablice
 create table smjerovi(
 sifra int not null primary key identity(1,1),
 naziv varchar(50) not null,
-cijena decimal(18,2) null,
+cijena decimal(18,2) null, -- null se ne mora pisati
 izvodiseod datetime,
 vaucer bit
 );
@@ -28,6 +27,12 @@ velicinagrupe int not null,
 predavac varchar(50),
 smjer int not null references smjerovi(sifra)
 );
+
+-- razlika varchar i char
+-- varchar(10)
+-- 'Ana'
+-- char(10)
+-- 'Ana       '
 
 create table polaznici(
 sifra int not null primary key identity(1,1),
@@ -41,3 +46,4 @@ create table clanovi(
 grupa int not null references grupe(sifra),
 polaznik int not null references polaznici(sifra)
 );
+
